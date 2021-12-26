323c6c7d6c12e05d19853489fcc4884dc7bcbf79
Wanderson <wandersoncoder@gmail.com>
2021-12-26 14:14:57 -0300

    feat: rm eslint

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ module.exports = {
   future: {
     webpack5: true,
   },
-}
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
